698ba1aa5b4e18e4f8e062c997be565a4776a309
Avisek Das <avisekdas555@gmail.com>
2024-05-31 22:54:53 +0530

    refactoring and some improvements

--- src/components/CarouWheel.tsx
+++ src/components/CarouWheel.tsx
@@ -5,32 +5,60 @@ import useElementSize from '../hooks/useElementSize'
 import { useCallback, useEffect, useMemo, useState } from 'react'
 import bezier from 'bezier-easing'
 
-function calculateThetaFromArcLength(L: number, r: number): number {
-  const thetaRadians = L / r
-  const thetaDegrees = thetaRadians * (180 / Math.PI)
-  return thetaDegrees
+const DEGREES_TO_RADIANS = Math.PI / 180
+const RADIANS_TO_DEGREES = 180 / Math.PI
+
+function degreesToArcLength(angle: number, radius: number) {
+  const thetaRadians = angle * DEGREES_TO_RADIANS
+  return radius * thetaRadians
 }
 
-function calculateArcLength(R: number, r: number): number {
-  let cosTheta = (2 * r * r - R * R) / (2 * r * r)
+function arcLengthToDegrees(arcLength: number, radius: number) {
+  const thetaRadians = arcLength / radius
+  return thetaRadians * RADIANS_TO_DEGREES
+}
+
+function calculateArcLengthOccupiedByACircleOnACircle(
+  primaryCircleRadius: number,
+  auxiliaryCircleRadius: number,
+) {
+  let cosTheta =
+    (2 * auxiliaryCircleRadius ** 2 - primaryCircleRadius ** 2) /
+    (2 * auxiliaryCircleRadius ** 2)
 
   if (cosTheta < -1) cosTheta = -1
   if (cosTheta > 1) cosTheta = 1
 
   const theta = Math.acos(cosTheta / 2)
 
-  const arcLength = r * theta
+  const arcLength = auxiliaryCircleRadius * theta
   return arcLength
 }
 
 export default function CarouWheel() {
-  const slides = ['One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven']
+  const slides = [
+    'One',
+    'Two',
+    'Three',
+    'Four',
+    'Five',
+    'Six',
+    'Seven',
+    // 'Eight',
+    // 'Nine',
+    // 'Ten',
+    // 'Eleven',
+    // 'Twelve',
+    // 'Thirteen',
+    // 'Fourteen',
+  ]
 
   const scaleEasing = useMemo(() => bezier(1, 0, 0.8, 0.8), [])
 
   const [containerRef, containerSize] = useElementSize()
 
-  const circleBaseSize = (40 / 448) * containerSize.height
+  // const circleBaseSize = 40
+  const circleBaseSize = (40 / 328) * containerSize.height
 
   const circleScaleMin = 1
   const circleScaleMax = 3
@@ -69,7 +97,7 @@ export default function CarouWheel() {
 
     const circleSize = circleBaseSize * circleScale
 
-    const arcLength = calculateArcLength(
+    const arcLength = calculateArcLengthOccupiedByACircleOnACircle(
       containerSize.width / 2,
       circleSize / 2,
     )
@@ -83,30 +111,32 @@ export default function CarouWheel() {
     }
   })
 
-  const totalArcLength = circles.reduce(
+  const totalCirclesArcLength = circles.reduce(
     (total, circle) => total + circle.arcLength,
     0,
   )
 
-  const bigCircleCircumference = (2 * Math.PI * containerSize.width) / 2
+  const space = 50
 
-  const availableArcLength = (bigCircleCircumference * 180) / 360
+  const gravityAngle = 0
 
-  const totalSpace = availableArcLength - totalArcLength
+  const totalArcLength = totalCirclesArcLength + space * (circles.length + 1)
 
-  const space = 0 && totalSpace / (circles.length + 1)
+  let currentPosition =
+    -totalArcLength / 2 +
+    degreesToArcLength(gravityAngle, containerSize.width / 2)
 
-  let currentArcLength = 0
+  currentPosition += space / 2
   const positionedCircles = circles.map((circle) => {
-    currentArcLength += space / 2
-    currentArcLength += circle.arcLength / 2
+    currentPosition += space / 2
+    currentPosition += circle.arcLength / 2
 
-    const position = currentArcLength - totalArcLength / 2
+    const position = currentPosition
 
-    currentArcLength += circle.arcLength / 2
-    currentArcLength += space / 2
+    currentPosition += circle.arcLength / 2
+    currentPosition += space / 2
 
-    const theta = calculateThetaFromArcLength(position, containerSize.width / 2)
+    const theta = arcLengthToDegrees(position, containerSize.width / 2)
 
     return {
       ...circle,
@@ -114,6 +144,7 @@ export default function CarouWheel() {
       theta,
     }
   })
+  currentPosition += space / 2
 
   const margin = (circleBaseSize * circleScaleMax) / 2
 
